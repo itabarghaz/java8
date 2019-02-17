8d91ed1fc63d5d5f7b76a7cfdf03506a55f5e335
abarghaz <abarghaztarik@gmail.com>
2019-02-17 01:26:42 +0100

    Documentation

--- src/apijava8/Main.java
+++ src/apijava8/Main.java
@@ -2,28 +2,42 @@ package apijava8;
 
 public class Main {
     public static void main(String[] args){
-        //Predicate<String> p1 = s -> s.length() < 20;
 
-        Predicate<String> p1 = new Predicate<String>() {
+        /**
+         * Définit deux regles
+         *
+         * X > 5 & X < 20
+         *
+         * Return predicate is boolean
+         *
+         * in this example we implement our function of interface Predicate
+         */
+
+        /*Predicate<String> p1_lessthan20 = new Predicate<String>() {
             @Override
             public boolean test(String s) {
                 return s.length() < 20;
             }
-        };
-       // Predicate<String> p1 = s -> s.length() < 20;
+        };*/
 
-        Predicate<String> p2 = s -> s.length() > 5;
+        Predicate<String> p1_lessthan20 = s -> s.length() < 20;
 
-        Predicate<String> p3 = p1.and(p2);
+        Predicate<String> p2_greaterthan = s -> s.length() > 5;
 
-        System.out.println("p3 for yes " + p3.test("hello0"));
+        Predicate<String> p3_and = p1_lessthan20.and(p2_greaterthan);
 
-        Predicate<String> p4 = p1.or(p2);
+        // Example for and
+        System.out.println("p3_and : > 5 and < 20 ===> " + p3_and.test("hel"));
 
-        System.out.println("p3 for yes " + p3.test("hello0"));
+        Predicate<String> p4_or = p1_lessthan20.or(p2_greaterthan);
 
-        Predicate<String> p5 = Predicate.isEqualsTo("yes");
-        System.out.println("p3 for yes " + p5.test("yes"));
-        System.out.println("p3 for no " + p5.test("no"));
+        // Example for or
+        System.out.println("p4_or : > 5 or < 20 ===> " + p4_or.test("he"));
+
+        Predicate<String> p5_equals = Predicate.isEqualsTo("yes");
+
+        // Example for equals
+        System.out.println("p5_equals for yes ===> " + p5_equals.test("yes"));
+        System.out.println("p5_equals for no ===> " + p5_equals.test("no"));
     }
 }
--- src/apijava8/Predicate.java
+++ src/apijava8/Predicate.java
@@ -4,6 +4,8 @@ package apijava8;
 public interface Predicate<T> {
     boolean test(T t);
     default Predicate<T> and(Predicate<T> other){
+        // this.test(var1) < 20
+        // other.test(var1) > 5
         return var1 -> this.test(var1) && other.test(var1);
     }
     default Predicate<T> or(Predicate<T> other){
